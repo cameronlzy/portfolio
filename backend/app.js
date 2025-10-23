8e8d0a408ca47fe221a5dd36abe66dd709f436f1
Cameron <cameronlzy@gmail.com>
2025-10-23 21:54:20 +0800

    Add deps

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const app = express()
 
 // 1) Middleware
 const corsConfig = {
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL,
   credentials: true,
   methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
